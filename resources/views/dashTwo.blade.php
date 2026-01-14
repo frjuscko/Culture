4ef23f0a816ef7ade5650aa29f0ff2648ba19d8c
Frégis Cakpo <ckpfrejus@gmail/com>
2026-01-14 16:30:44 +0100

    Commit 15

--- resources/views/dashTwo.blade.php
+++ resources/views/dashTwo.blade.php
@@ -13,7 +13,7 @@
         <div class="part-left">
             <div class="logo">
         <a href="{{ route('home') }}">
-          <img src="{{ URL::asset('logo/logo.png') }}" alt="">
+          <img src="{{ asset('logo/logo_cb.png') }}" alt="">
         </a>
       </div>
         </div>
