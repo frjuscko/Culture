9f72065aaaee0944364273a97a3c8fa888576bed
Frégis Cakpo <ckpfrejus@gmail/com>
2026-01-14 01:03:30 +0100

    commit 2

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ URL::asset('frontend/css/style.css') }}">
-    <script src="{{ URL::asset('frontend/js/navbar.js') }}" defer></script>
+    <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
+    <script src="{{ asset('frontend/js/navbar.js') }}" defer></script>
     <title>Document</title>
 </head>
 
